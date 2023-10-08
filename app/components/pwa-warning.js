d075ef6dcee2a7d5e4a26f8c103bf39f3d913063
ciberdown <teymuri.amin@gmail.com>
2023-10-09 00:59:56 +0330

    warning added

---
+++ app/components/pwa-warning.js
@@ -0,0 +1,14 @@
+function PwaWarning() {
+  return (
+    <div className="pwa-warning">
+      <h1>this is a PWA app</h1>
+      <div>
+        <p className="left-arr">{">>>>"}</p>
+        <h2>change the screen size</h2>
+        <p className="right-arr">{"<<<<"}</p>
+      </div>
+    </div>
+  );
+}
+
+export default PwaWarning;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import "./app.scss";
+import PwaWarning from "./components/pwa-warning";
 import Start from "./start/page";
 
 export default function Home() {
   return (
     <main className="app">
+      <PwaWarning />
+
       <Start />
     </main>
   );
